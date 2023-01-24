ccfbacac938ad8fc8f6bf78ba232ddf085f6cf62
sofiagakalo1 <sofiagakalo1@gmail.com>
2023-01-24 21:45:12 +0100

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    name: '',
+    number: '',
+  };
+
+  addContact = ({ name, number }) => {
+    this.setState(prevState => {
+      return [{ name, number, id: nanoid() }, ...prevState.contacts];
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div>
+          <h2>Phonebook</h2>
+          <div>
+            <p>Name</p>
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </div>
+          <div>
+            <p>Number</p>
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </div>
+          <div>
+            <button type="submit" onSubmit={() => this.addContact()}>
+              Add contact
+            </button>
+          </div>
+        </div>
+        <div>
+          <h3>Contacts</h3>
+          <p>Find contacts by name</p>
+          <input></input>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </>
+    );
+  }
+}
